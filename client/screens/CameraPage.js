0bfc0ae73a80cf6fb56b61e6ad8a9c1cacb9d2f8
jiwoorld <bncmdbev@gmail.com>
2023-08-06 20:41:26 +0900

    ➕ expo-camera 모듈 설치

---
+++ client/screens/CameraPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+
+const CameraPage = ({ navigation }) => {
+
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default CameraPage;
