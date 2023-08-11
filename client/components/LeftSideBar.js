34d95e8fd403b33800a722cb9bbf428ccf1ac6eb
godori1012 <rhdmstjrjdi@naver.com>
2023-08-11 14:42:31 +0900

    💄 EventPage에 왼쪽 가게리스트 고정

---
+++ client/components/LeftSideBar.js
@@ -0,0 +1,96 @@
+import React from "react";
+import {
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  StyleSheet,
+} from "react-native";
+import removeStore from "../assets/icons/removeStore.png";
+import plusStore from "../assets/icons/plusStore.png";
+import cozy from "../assets/icons/cozy.png";
+import setting from "../assets/icons/setting.png";
+
+const LeftSidebar = ({
+  isAddMode,
+  stores,
+  editMode,
+  deleteStore,
+  navigation,
+}) => {
+  return (
+    <View style={styles.allStore}>
+      {stores.map((store) => (
+        <TouchableWithoutFeedback
+          key={store.id}
+          onPress={() => deleteStore(store.id)}
+        >
+          <View>
+            <Image source={cozy} style={styles.clkStore} />
+            {isAddMode && <Image source={removeStore} style={styles.rmStore} />}
+          </View>
+        </TouchableWithoutFeedback>
+      ))}
+
+      {isAddMode && (
+        <TouchableWithoutFeedback
+          onPress={() => navigation.navigate("SearchPage")}
+        >
+          <View>
+            <Image source={plusStore} style={styles.plsStore} />
+          </View>
+        </TouchableWithoutFeedback>
+      )}
+
+      <TouchableWithoutFeedback onPress={editMode}>
+        <View style={styles.settingContainer}>
+          <Image source={setting} style={styles.setting} />
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  allStore: {
+    width: 80,
+    height: "100%",
+    backgroundColor: "#C4D7E0",
+  },
+  clkStore: {
+    width: 53,
+    height: 53,
+    borderRadius: 53,
+    borderWidth: 1.5,
+    borderColor: "#6E85B7",
+
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 13,
+  },
+  rmStore: {
+    position: "absolute",
+    top: 9,
+    left: 50,
+    width: 24,
+    height: 24,
+  },
+  plsStore: {
+    width: 54,
+    height: 54,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 16,
+  },
+  setting: {
+    width: 30,
+    height: 30,
+    marginBottom: 25.63,
+  },
+  settingContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
+
+export default LeftSidebar;
