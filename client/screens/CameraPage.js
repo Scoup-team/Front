c5c51b91e0d7b72a155c94e2b7309ca4b63d8457
jiwoorld <bncmdbev@gmail.com>
2023-09-05 23:44:29 +0900

    🚧 영수증 API 연결

--- client/screens/CameraPage.js
+++ client/screens/CameraPage.js
@@ -1,116 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import {
-//   Image,
-//   Text,
-//   View,
-//   TouchableOpacity,
-//   PermissionsAndroid,
-// } from "react-native";
-// import { Camera } from "expo-camera";
-// import { launchCamera, launchImageLibrary } from "react-native-image-picker";
-// import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
-
-// import { postReceipt } from "../api/receipt";
-
-// const CameraPage = () => {
-//   const [hasPermission, setHasPermission] = useState(null);
-//   const [cameraRef, setCameraRef] = useState(null);
-
-//   const getCameraPermission = async () => {
-//     const { status } = await Camera.requestCameraPermissionsAsync();
-//     if (status !== "granted") {
-//       console.log("No access to camera");
-//       return false;
-//     }
-//     return true;
-//   };
-
-//   useEffect(() => {
-//     (async () => {
-//       const hasPermission = await getCameraPermission();
-//       setHasPermission(hasPermission);
-//     })();
-//   }, []);
-
-//   const takePicture = async () => {
-//     if (cameraRef) {
-//       try {
-//         const photo = await cameraRef.takePictureAsync();
-//         console.log("photo", photo);
-
-//         // 이미지 압축
-//         try {
-//           const resizedImage = await manipulateAsync(
-//             photo.uri,
-//             [{ resize: { width: 300, height: 400 } }],
-//             { compress: 0.7, format: SaveFormat.JPEG }
-//           );
-//           console.log("Resized image:", resizedImage);
-
-//           const formData = new FormData();
-//           formData.append("file", {
-//             uri: resizedImage.uri,
-//             name: "photo.jpg",
-//             type: "image/jpeg",
-//           });
-
-//           await postReceipt(formData);
-
-//           console.log("Resized image.uri", resizedImage.uri);
-//         } catch (error) {
-//           console.error("Error resizing image: ", error);
-//         }
-//       } catch (error) {
-//         console.log("Error taking picture: ", error);
-//       }
-//     }
-//   };
-
-//   if (hasPermission === null) {
-//     return <View />;
-//   }
-//   if (hasPermission === false) {
-//     return <Text>No access to camera</Text>;
-//   }
-
-//   return (
-//     <View style={{ flex: 1 }}>
-//       <Image source={{ uri: photo }} />
-//       <Button title="이미지 선택" onPress={showPicker}></Button>
-//       <Camera
-//         style={{ flex: 1 }}
-//         type={Camera.Constants.Type.back}
-//         ref={(ref) => setCameraRef(ref)}
-//       >
-//         <View
-//           style={{
-//             flex: 1,
-//             backgroundColor: "transparent",
-//             justifyContent: "flex-end",
-//             alignItems: "center",
-//           }}
-//         >
-//           <TouchableOpacity
-//             style={{
-//               width: 60,
-//               height: 60,
-//               borderRadius: 40,
-//               backgroundColor: "white",
-//               justifyContent: "center",
-//               alignItems: "center",
-//               marginBottom: 20,
-//             }}
-//             onPress={takePicture}
-//           >
-//             <Text style={{ fontSize: 14 }}>촬영</Text>
-//           </TouchableOpacity>
-//         </View>
-//       </Camera>
-//     </View>
-//   );
-// };
-
-// export default CameraPage;
 import React, { useState, useEffect, useRef } from "react";
 import {
   View,
@@ -122,9 +9,11 @@ import {
   StyleSheet,
 } from "react-native";
 import { Camera } from "expo-camera";
+import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
 import { launchImageLibrary, launchCamera } from "react-native-image-picker";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
+import { postReceipt } from "../api/receipt";
 
 const imagePickerOption = {
   mediaType: "photo",
@@ -159,7 +48,29 @@ const CameraPage = () => {
       try {
         const photo = await cameraRef.takePictureAsync();
         console.log("photo", photo);
-        setPhoto(photo);
+
+        // 이미지 압축
+        try {
+          const resizedImage = await manipulateAsync(
+            photo.uri,
+            [{ resize: { width: 300, height: 400 } }],
+            { compress: 0.7, format: SaveFormat.JPEG }
+          );
+          console.log("Resized image:", resizedImage);
+
+          const formData = new FormData();
+          formData.append("file", {
+            uri: resizedImage.uri,
+            name: "photo.jpg",
+            type: "image/jpeg",
+          });
+
+          await postReceipt(formData);
+
+          console.log("Resized image.uri", resizedImage.uri);
+        } catch (error) {
+          console.error("Error resizing image: ", error);
+        }
       } catch (error) {
         console.log("Error taking picture: ", error);
       }
@@ -176,31 +87,42 @@ const CameraPage = () => {
 
   const pickImageFromGallery = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    console.log("status", status);
 
     if (status !== "granted") {
       console.log("Gallery permission denied");
       return;
     }
 
-    launchImageLibrary(imagePickerOption, (res) => {
-      if (res.didCancel || !res.uri) {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync(
+        imagePickerOption
+      );
+      const selectedImageAssets = result.assets;
+      if (!selectedImageAssets || selectedImageAssets.length === 0) {
         closeModal();
         return;
       }
-      setPhoto(res);
-      closeModal();
-    });
-  };
+      const resizedImage = await manipulateAsync(
+        selectedImageAssets[0].uri,
+        [{ resize: { width: 300, height: 400 } }],
+        { compress: 0.7, format: SaveFormat.JPEG }
+      );
+      // 선택한 이미지를 서버로 업로드
+      const formData = new FormData();
+      formData.append("file", {
+        uri: resizedImage.uri,
+        name: "photo.jpg",
+        type: "image/jpeg",
+      });
 
-  const launchCameraAndPickImage = () => {
-    launchCamera(imagePickerOption, (res) => {
-      if (res.didCancel || !res.uri) {
-        closeModal();
-        return;
-      }
-      setPhoto(res);
+      await postReceipt(formData);
+
+      console.log("Image uploaded successfully");
       closeModal();
-    });
+    } catch (error) {
+      console.error("Error picking an image: ", error);
+    }
   };
 
   if (hasPermission === null) {
@@ -212,7 +134,6 @@ const CameraPage = () => {
 
   return (
     <View style={{ flex: 1 }}>
-      {photo && <Image source={{ uri: photo.uri }} style={{ flex: 1 }} />}
       <Camera
         style={{ flex: 1 }}
         type={Camera.Constants.Type.back}
@@ -240,25 +161,13 @@ const CameraPage = () => {
           >
             <Text style={{ fontSize: 14 }}>촬영</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={openModal}>
+          <TouchableOpacity onPress={pickImageFromGallery}>
             <Text style={{ fontSize: 18, color: "white" }}>
               갤러리에서 이미지 선택
             </Text>
           </TouchableOpacity>
         </View>
       </Camera>
-      <Modal visible={modalVisible} animationType="slide">
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <Pressable onPress={pickImageFromGallery}>
-            <Text style={{ fontSize: 18 }}>갤러리에서 이미지 선택</Text>
-          </Pressable>
-          <Pressable onPress={closeModal}>
-            <Text style={{ fontSize: 18, color: "red" }}>닫기</Text>
-          </Pressable>
-        </View>
-      </Modal>
     </View>
   );
 };
