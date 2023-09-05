61903d47a914b041db924166f0187c75a213cfa5
jiwoorld <bncmdbev@gmail.com>
2023-09-05 22:14:34 +0900

    ➕ image-picker 라이브러리 설치

--- client/api/receipt.js
+++ client/api/receipt.js
@@ -1,15 +1,15 @@
+import axios from 'axios';
 import { AI_SPRING_URL } from "./url";
 
 //영수증 이미지를 보내는 API
 export const postReceipt = async (formData) => {
   try {
-    const response = await AI_SPRING_URL.post(`receipt`, formData, {
+    const config = {
       headers: {
         "Content-Type": "multipart/form-data",
       },
-      // transformRequest를 수정하여 formData를 반환
-      transformRequest: [(data, headers) => formData],
-    });
+    };
+    const response = await axios.post(`http://118.67.129.165/receipt`, formData, config);
     console.log("Response:", response.data);
   } catch (err) {
     console.error("Error posting receipt: ", err);
--- client/screens/CameraPage.js
+++ client/screens/CameraPage.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
+import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
+
 import { postReceipt } from "../api/receipt";
 
 const CameraPage = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
-  const [data, setData] = useState([]);
 
   const getCameraPermission = async () => {
     const { status } = await Camera.requestCameraPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('No access to camera');
+    if (status !== "granted") {
+      console.log("No access to camera");
       return false;
     }
     return true;
   };
-  
+
   useEffect(() => {
     (async () => {
       const hasPermission = await getCameraPermission();
@@ -30,17 +31,28 @@ const CameraPage = () => {
         const photo = await cameraRef.takePictureAsync();
         console.log("photo", photo);
 
-        const formData = new FormData();
-        formData.append("file", {
-          uri: photo.uri,
-          name: "photo.jpg",
-          type: "image/jpeg",
-        });
+        // 이미지 압축
+        try {
+          const resizedImage = await manipulateAsync(
+            photo.uri,
+            [{ resize: { width: 300, height: 400 } }],
+            { compress: 0.7, format: SaveFormat.JPEG }
+          );
+          console.log("Resized image:", resizedImage);
 
-        await postReceipt(formData);
+          const formData = new FormData();
+          formData.append("file", {
+            uri: resizedImage.uri,
+            name: "photo.jpg",
+            type: "image/jpeg",
+          });
 
-        // 이 코드는 postReceipt 함수가 완료된 후 실행되도록 이동
-        console.log("photo.uri", photo.uri);
+          await postReceipt(formData);
+
+          console.log("Resized image.uri", resizedImage.uri);
+        } catch (error) {
+          console.error("Error resizing image: ", error);
+        }
       } catch (error) {
         console.log("Error taking picture: ", error);
       }
